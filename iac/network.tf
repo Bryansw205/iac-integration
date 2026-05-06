39c98e17f2571a99c008f94f6407ad9713e6502d
Bryan Mariños <bryanmg.020@gmail.com>
2026-05-06 01:24:22 -0500

    feat: Añadi redes publicas y enrutamiento

--- iac/network.tf
+++ iac/network.tf
@@ -56,3 +56,102 @@ resource "aws_security_group" "vpce_sqs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = { Name = "igw-${var.entorno}" }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}-a"
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet-a-${var.entorno}" }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.region}-b"
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet-b-${var.entorno}" }
+}
+
+resource "aws_eip" "nat_a" {
+  domain = "vpc"
+  tags   = { Name = "eip-nat-a-${var.entorno}" }
+}
+
+resource "aws_eip" "nat_b" {
+  domain = "vpc"
+  tags   = { Name = "eip-nat-b-${var.entorno}" }
+}
+
+resource "aws_nat_gateway" "nat_a" {
+  allocation_id = aws_eip.nat_a.id
+  subnet_id     = aws_subnet.public_a.id
+  tags          = { Name = "nat-gateway-a-${var.entorno}" }
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "nat_b" {
+  allocation_id = aws_eip.nat_b.id
+  subnet_id     = aws_subnet.public_b.id
+  tags          = { Name = "nat-gateway-b-${var.entorno}" }
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_a.id
+  }
+
+  tags = { Name = "private-rt-a-${var.entorno}" }
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_b.id
+  }
+
+  tags = { Name = "private-rt-b-${var.entorno}" }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = { Name = "public-rt-${var.entorno}" }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
