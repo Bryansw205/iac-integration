2c16853467f697f304aaf8eb73e3f5248c2bd866
Bryan Mariños <bryanmg.020@gmail.com>
2026-05-06 01:32:17 -0500

    fix: Enrutar S3 en las tablas

--- iac/network.tf
+++ iac/network.tf
@@ -24,7 +24,10 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids = [aws_vpc.main.default_route_table_id]
+  route_table_ids = [
+    aws_route_table.private_a.id,
+    aws_route_table.private_b.id
+  ]
 }
 
 resource "aws_vpc_endpoint" "sqs" {
