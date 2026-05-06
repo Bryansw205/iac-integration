d3ab32d279564057d8ab1e17617e90ce3673a577
Bryan Mariños <bryanmg.020@gmail.com>
2026-05-06 00:35:02 -0500

    feat: Añadir variables

---
+++ iac/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    description: "Region de despliegue"
+    type: string
+    default: us-east-1
+}
+
+variable "entorno" {
+    description = "Entorno de despliegue"
+    type = string
+}
+
+variable "vpc-cidr" {
+    description = "CIDR de la VPC"
+    type = string
+    default = "10.0.0.0/16"
+}
