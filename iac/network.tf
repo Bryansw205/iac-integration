e2d4a0ce243b12a07e8b85eb6a1aa72f4bd2d678
Bryan Mariños <bryanmg.020@gmail.com>
2026-05-06 18:02:06 -0500

    fix: Arreglar sintaxis

--- iac/network.tf
+++ iac/network.tf
@@ -9,14 +9,14 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.11.0/24"
-  availability_zone = "${var.region}-a"
+  availability_zone = "${var.region}a"
   tags              = { Name = "private-subnet-a-${var.entorno}" }
 }
 
 resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.12.0/24"
-  availability_zone = "${var.region}-b"
+  availability_zone = "${var.region}b"
   tags              = { Name = "private-subnet-b-${var.entorno}" }
 }
 
@@ -50,7 +50,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "${var.region}-a"
+  availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
   tags = { Name = "public-subnet-a-${var.entorno}" }
 }
@@ -58,7 +58,7 @@ resource "aws_subnet" "public_a" {
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "${var.region}-b"
+  availability_zone       = "${var.region}b"
   map_public_ip_on_launch = true
   tags = { Name = "public-subnet-b-${var.entorno}" }
 }
