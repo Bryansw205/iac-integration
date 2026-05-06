670f0e8158838f2ecdec25bd8a9bad1a5bf00825
Bryan Mariños <bryanmg.020@gmail.com>
2026-05-06 03:51:56 -0500

    fix: Eliminar segurity group duplicado

--- iac/network.tf
+++ iac/network.tf
@@ -41,25 +41,6 @@ resource "aws_vpc_endpoint" "sqs" {
   tags = { Name = "vpce-sqs-${var.entorno}" }
 }
 
-resource "aws_security_group" "vpce_sqs_sg" {
-  name        = "image-processor-vpce-sqs-sg-${var.entorno}"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
